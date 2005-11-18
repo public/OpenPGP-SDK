5d8a7379eb06497b0c03264987b35f6c972fb141
ben <ben@228a932f-eeec-0310-9813-fb354622df21>
2005-11-18 06:34:32 +0000

    Refactor.
    git-svn-id: svn://openpgp.nominet.org.uk/openpgpsdk/trunk@297 228a932f-eeec-0310-9813-fb354622df21

---
+++ examples/common.c
@@ -0,0 +1,41 @@
+#include "common.h"
+#include <openpgpsdk/util.h>
+#include <memory.h>
+#include <fcntl.h>
+#include <assert.h>
+
+static ops_parse_callback_return_t
+callback(const ops_parser_content_t *content,void *arg_)
+    {
+    ops_secret_key_t **skey=arg_;
+
+    if(content->tag == OPS_PTAG_CT_SECRET_KEY)
+	{
+	memcpy(skey,&content->content.secret_key,sizeof skey);
+	return OPS_KEEP_MEMORY;
+	}
+
+    return OPS_RELEASE_MEMORY;
+    }
+    
+ops_secret_key_t *get_key(const char *keyfile)
+    {
+    ops_reader_fd_arg_t arg;
+    ops_parse_info_t parse_info;
+    ops_secret_key_t *skey;
+
+    ops_parse_info_init(&parse_info);
+    parse_info.cb=callback;
+
+    arg.fd=open(keyfile,O_RDONLY);
+    assert(arg.fd >= 0);
+    parse_info.reader_arg=&arg;
+    parse_info.reader=ops_reader_fd;
+
+    skey=NULL;
+    parse_info.cb_arg=&skey;
+
+    ops_parse(&parse_info);
+
+    return skey;
+    }
--- examples/create-signed-key.c
+++ examples/create-signed-key.c
@@ -1,3 +1,4 @@
+#include "common.h"
 #include <openpgpsdk/create.h>
 #include <openpgpsdk/util.h>
 #include <openpgpsdk/signature.h>
@@ -13,42 +14,6 @@
  * is because we don't have a way to generate our own (yet).
  */
 
-static ops_secret_key_t skey;
-static ops_boolean_t skey_found;
-
-static ops_parse_callback_return_t
-callback(const ops_parser_content_t *content,void *arg_)
-    {
-    OPS_USED(arg_);
-
-    if(content->tag == OPS_PTAG_CT_SECRET_KEY)
-	{
-	memcpy(&skey,&content->content.secret_key,sizeof skey);
-	skey_found=ops_true;
-	return OPS_KEEP_MEMORY;
-	}
-
-    return OPS_RELEASE_MEMORY;
-    }
-    
-static void get_key(const char *keyfile)
-    {
-    ops_reader_fd_arg_t arg;
-    ops_parse_info_t parse_info;
-
-    ops_parse_info_init(&parse_info);
-    parse_info.cb=callback;
-
-    arg.fd=open(keyfile,O_RDONLY);
-    assert(arg.fd >= 0);
-    parse_info.reader_arg=&arg;
-    parse_info.reader=ops_reader_fd;
-
-    ops_parse(&parse_info);
-
-    assert(skey_found);
-    }
-
 int main(int argc,char **argv)
     {
     ops_writer_fd_arg_t arg;
@@ -58,6 +23,8 @@ int main(int argc,char **argv)
     unsigned char keyid[OPS_KEY_ID_SIZE];
     unsigned char *user_id; /* not const coz we use _fast_ */
     const char *keyfile;
+    ops_secret_key_t *skey;
+
 
     if(argc != 3)
 	{
@@ -70,30 +37,31 @@ int main(int argc,char **argv)
 
     ops_init();
 
-    get_key(keyfile);
+    skey=get_key(keyfile);
+    assert(skey);
 
     arg.fd=1;
     opt.writer=ops_writer_fd;
     opt.arg=&arg;
 
-    ops_write_struct_public_key(&skey.public_key,&opt);
+    ops_write_struct_public_key(&skey->public_key,&opt);
 
     ops_fast_create_user_id(&id,user_id);
     ops_write_struct_user_id(&id,&opt);
 
-    ops_signature_start(&sig,&skey.public_key,&id,OPS_CERT_POSITIVE);
+    ops_signature_start(&sig,&skey->public_key,&id,OPS_CERT_POSITIVE);
     ops_signature_add_creation_time(&sig,time(NULL));
 
-    ops_keyid(keyid,&skey.public_key);
+    ops_keyid(keyid,&skey->public_key);
     ops_signature_add_issuer_key_id(&sig,keyid);
 
     ops_signature_add_primary_user_id(&sig,ops_true);
 
     ops_signature_hashed_subpackets_end(&sig);
 
-    ops_write_signature(&sig,&skey.public_key,&skey,&opt);
+    ops_write_signature(&sig,&skey->public_key,skey,&opt);
 
-    ops_secret_key_free(&skey);
+    ops_secret_key_free(skey);
 
     ops_finish();
 
