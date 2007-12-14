a5c6cd84e507555caae76f8c12c8a5002d977938
rachel <rachel@228a932f-eeec-0310-9813-fb354622df21>
2007-12-14 11:41:50 +0000

    Added test to verify failure on bad sig
    git-svn-id: svn://openpgp.nominet.org.uk/openpgpsdk/trunk@528 228a932f-eeec-0310-9813-fb354622df21

--- tests/test_rsa_verify.c
+++ tests/test_rsa_verify.c
@@ -30,9 +30,13 @@ static char *filename_rsa_armour_passphrase="gpg_signed_armour_passphrase.txt";
 
 static char *filename_rsa_noarmour_nopassphrase="gpg_signed_noarmour_nopassphrase.txt";
 static char *filename_rsa_noarmour_passphrase="gpg_signed_noarmour_passphrase.txt";
+static char *filename_rsa_noarmour_fail_bad_sig="gpg_signed_noarmour_fail_bad_sig.txt";
 
 static char *filename_rsa_clearsign_nopassphrase="gpg_clearsigned_nopassphrase.txt";
 static char *filename_rsa_clearsign_passphrase="gpg_clearsigned_passphrase.txt";
+static char *filename_rsa_clearsign_fail_bad_sig="gpg_clearsigned_fail_bad_sig.txt";
+
+typedef ops_parse_cb_return_t (*ops_callback)(const ops_parser_content_t *, ops_parse_cb_info_t *);
 
 /* Signature verification suite initialization.
  * Create temporary test files.
@@ -49,6 +53,7 @@ int init_suite_rsa_verify(void)
 
     create_testfile(filename_rsa_noarmour_nopassphrase);
     create_testfile(filename_rsa_noarmour_passphrase);
+    create_testfile(filename_rsa_noarmour_fail_bad_sig);
 
     // Now sign the test files with GPG
 
@@ -72,12 +77,18 @@ int init_suite_rsa_verify(void)
     if (system(cmd))
         { return 1; }
 
+    snprintf(cmd,MAXBUF,"%s --openpgp --compress-level 0 --sign --local-user %s %s/%s",
+             gpgcmd, alpha_name, dir, filename_rsa_noarmour_fail_bad_sig);
+    if (system(cmd))
+        { return 1; }
+
     /*
      * Create CLEARSIGNED test files
      */
 
     create_testfile(filename_rsa_clearsign_nopassphrase);
     create_testfile(filename_rsa_clearsign_passphrase);
+    create_testfile(filename_rsa_clearsign_fail_bad_sig);
 
     // and sign them
 
@@ -90,6 +101,13 @@ int init_suite_rsa_verify(void)
              gpgcmd, bravo_name, bravo_passphrase, dir, filename_rsa_clearsign_passphrase);
     if (system(cmd))
         { return 1; }
+
+    snprintf(cmd,MAXBUF,"%s --openpgp --compress-level 0 --clearsign --textmode --local-user %s --armor %s/%s",
+             gpgcmd, alpha_name, dir, filename_rsa_clearsign_fail_bad_sig);
+    if (system(cmd))
+        { return 1; }
+    // sig will be turned bad on verification
+    // \todo make sig bad here instead
 
     // Return success
     return 0;
@@ -104,21 +122,18 @@ int clean_suite_rsa_verify(void)
     return 0;
     }
 
-static void test_rsa_verify(const int has_armour, const int has_passphrase ATTRIBUTE_UNUSED, const char *filename, const char* protocol)
+static int test_rsa_verify(const int has_armour, const char *filename, ops_callback callback, ops_parse_info_t *pinfo)
     {
     char signedfile[MAXBUF+1];
     char *suffix= has_armour ? "asc" : "gpg";
     int fd=0;
-    ops_parse_info_t *pinfo=NULL;
     validate_data_cb_arg_t validate_arg;
     ops_validate_result_t result;
     int rtn=0;
     
     // open signed file
-    snprintf(signedfile,MAXBUF,"%s/%s%s%s.%s",
+    snprintf(signedfile,MAXBUF,"%s/%s.%s",
              dir, filename,
-             protocol==NULL ? "" : "_",
-             protocol==NULL ? "" : protocol,
              suffix);
 #ifdef WIN32
     fd=open(signedfile,O_RDONLY | O_BINARY);
@@ -133,100 +148,193 @@ static void test_rsa_verify(const int has_armour, const int has_passphrase ATTRI
     
     // Set verification reader and handling options
 
-    pinfo=ops_parse_info_new();
-
     memset(&validate_arg,'\0',sizeof validate_arg);
     validate_arg.result=&result;
     validate_arg.keyring=&pub_keyring;
     validate_arg.rarg=ops_reader_get_arg_from_pinfo(pinfo);
 
-    ops_parse_cb_set(pinfo,callback_verify,&validate_arg);
+    ops_parse_cb_set(pinfo,callback,&validate_arg);
     ops_reader_set_fd(pinfo,fd);
     pinfo->rinfo.accumulate=ops_true;
 
-    // Set up armour/passphrase options
+    // Set up armour options
 
     if (has_armour)
         ops_reader_push_dearmour(pinfo,ops_false,ops_false,ops_false);
-    //    current_passphrase=has_passphrase ? passphrase : nopassphrase;
     
     // Do the verification
 
     rtn=ops_parse(pinfo);
-    ops_print_errors(ops_parse_info_get_errors(pinfo));
-    CU_ASSERT(rtn==1);
 
     // Tidy up
     if (has_armour)
-	ops_reader_pop_dearmour(pinfo);
-
-    ops_parse_info_delete(pinfo);
+        ops_reader_pop_dearmour(pinfo);
 
     close(fd);
-    
-#ifdef NEEDED
-    // File contents should match
-    create_testtext(filename,&testtext[0],MAXBUF);
-    CU_ASSERT(memcmp(literal_data,testtext,sz_literal_data)==0);
-#endif
+
+    return (rtn);
+    }
+
+static void test_rsa_verify_ok(const int has_armour, const char *filename)
+    {
+    int rtn=0;
+    ops_parse_info_t *pinfo=NULL;
+
+    // setup
+    pinfo=ops_parse_info_new();
+
+    // parse
+    rtn=test_rsa_verify(has_armour, filename, callback_verify, pinfo);
+
+    // handle result
+    ops_print_errors(ops_parse_info_get_errors(pinfo));
+    CU_ASSERT(rtn==1);
+
+    // clean up
+    ops_parse_info_delete(pinfo);
+    }
+
+
+static void test_rsa_verify_fail(const int has_armour, const char *filename, ops_callback callback, ops_errcode_t expected_errcode)
+    {
+    int rtn=0;
+    ops_parse_info_t *pinfo=NULL;
+    ops_callback cb=NULL;
+    ops_error_t* errstack=NULL;
+
+    cb = callback==NULL ? callback_verify : callback;
+
+    // setup
+    pinfo=ops_parse_info_new();
+
+    // parse
+    rtn=test_rsa_verify(has_armour, filename, cb, pinfo);
+
+    // handle result - should fail
+    errstack=ops_parse_info_get_errors(pinfo);
+    // we are expecting one and only one error
+    // print out errors if we have actually got a different error
+    CU_ASSERT(errstack!=NULL);
+
+    if (errstack && errstack->errcode!=expected_errcode)
+        {
+        ops_print_errors(errstack);
+        }
+    CU_ASSERT(rtn==0);
+
+    // clean up
+    ops_parse_info_delete(pinfo);
     }
 
 static void test_rsa_verify_noarmour_nopassphrase(void)
     {
-    //    int clearsign=0;
     int armour=0;
-    int passphrase=0;
     assert(pub_keyring.nkeys);
-    //    const ops_key_data_t *pub_key=ops_keyring_find_key_by_userid(&pub_keyring, alpha_user_id);
-    //    assert(pub_key);
-    test_rsa_verify(armour,passphrase,filename_rsa_noarmour_nopassphrase,NULL);
+
+    test_rsa_verify_ok(armour,filename_rsa_noarmour_nopassphrase);
     }
 
 static void test_rsa_verify_noarmour_passphrase(void)
     {
-    //    int clearsign=0;
     int armour=0;
-    int passphrase=1;
     assert(pub_keyring.nkeys);
-    test_rsa_verify(armour,passphrase,filename_rsa_noarmour_passphrase,NULL);
+    test_rsa_verify_ok(armour,filename_rsa_noarmour_passphrase);
     }
 
 static void test_rsa_verify_armour_nopassphrase(void)
     {
     int armour=1;
-    int passphrase=0;
     assert(pub_keyring.nkeys);
-    test_rsa_verify(armour,passphrase,filename_rsa_armour_nopassphrase,NULL);
+
+    test_rsa_verify_ok(armour,filename_rsa_armour_nopassphrase);
     }
 
 static void test_rsa_verify_armour_passphrase(void)
     {
     int armour=1;
-    int passphrase=1;
     assert(pub_keyring.nkeys);
-    test_rsa_verify(armour,passphrase,filename_rsa_armour_passphrase,NULL);
+
+    test_rsa_verify_ok(armour,filename_rsa_armour_passphrase);
     }
 
 static void test_rsa_verify_clearsign_nopassphrase(void)
     {
-    //    int clearsign=1;
     int armour=1;
-    int passphrase=0;
     assert(pub_keyring.nkeys);
 
-    test_rsa_verify(armour,passphrase,filename_rsa_clearsign_nopassphrase,NULL);
+    test_rsa_verify_ok(armour,filename_rsa_clearsign_nopassphrase);
     }
 
 static void test_rsa_verify_clearsign_passphrase(void)
     {
-    //    int clearsign=1;
     int armour=1;
-    int passphrase=1;
     assert(pub_keyring.nkeys);
 
-    test_rsa_verify(armour,passphrase,filename_rsa_clearsign_passphrase,NULL);
+    test_rsa_verify_ok(armour,filename_rsa_clearsign_passphrase);
     }
 
+static ops_parse_cb_return_t callback_bad_sig(const ops_parser_content_t* content_, ops_parse_cb_info_t *cbinfo)
+    {
+    int target;
+    unsigned char* data;
+    unsigned char orig;
+    switch (content_->tag)
+        {
+    case OPS_PTAG_CT_SIGNED_CLEARTEXT_BODY:
+    case OPS_PTAG_CT_LITERAL_DATA_BODY:
+        // change something in the signed text to break the sig
+        switch (content_->tag)
+            {
+        case OPS_PTAG_CT_SIGNED_CLEARTEXT_BODY:
+            target=content_->content.signed_cleartext_body.length;
+            orig=content_->content.signed_cleartext_body.data[target];
+            data=(unsigned char*) &content_->content.signed_cleartext_body.data[0];
+            break;
+
+        case OPS_PTAG_CT_LITERAL_DATA_BODY:
+            target=content_->content.literal_data_body.length;
+            orig=content_->content.literal_data_body.data[target];
+            data=(unsigned char*) &content_->content.literal_data_body.data[0];
+            break;
+
+        default:
+            assert(0);
+            }
+
+        if (target==0)
+            {
+            fprintf(stderr,"Nothing in text body to change!!\n");
+            break;
+            }
+
+        // change a byte somewhere near the middle
+        target/=2;
+        // remove const-ness so we can change it
+        data[target]= ~orig;
+        assert(orig!=content_->content.signed_cleartext_body.data[target]);
+        break;
+
+    default:
+        break;
+        }
+    return callback_verify(content_,cbinfo);
+    }
+
+static void test_rsa_verify_noarmour_fail_bad_sig(void)
+    {
+    int armour=0;
+    assert(pub_keyring.nkeys);
+
+    test_rsa_verify_fail(armour,filename_rsa_noarmour_fail_bad_sig,callback_bad_sig,OPS_E_V_BAD_SIGNATURE);
+    }
+
+static void test_rsa_verify_clearsign_fail_bad_sig(void)
+    {
+    int armour=1;
+    assert(pub_keyring.nkeys);
+
+    test_rsa_verify_fail(armour,filename_rsa_clearsign_fail_bad_sig,callback_bad_sig,OPS_E_V_BAD_SIGNATURE);
+    }
 
 CU_pSuite suite_rsa_verify()
 {
@@ -254,6 +362,11 @@ CU_pSuite suite_rsa_verify()
 	    return NULL;
     
     if (NULL == CU_add_test(suite, "Unarmoured, passphrase", test_rsa_verify_noarmour_passphrase))
+	    return NULL;
+
+    if (NULL == CU_add_test(suite, "Unarmoured: should fail on bad sig", test_rsa_verify_noarmour_fail_bad_sig))
+	    return NULL;
+    if (NULL == CU_add_test(suite, "Clearsign: should fail on bad sig", test_rsa_verify_clearsign_fail_bad_sig))
 	    return NULL;
 
     return suite;
